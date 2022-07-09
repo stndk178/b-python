4c54608d124d3ddd5ff38863f584585c37871fff
kosuah <ksa8847@naver.com>
2022-07-09 20:21:53 +0900

    2022-07-09 first commit

--- Class2/11050.py
+++ Class2/11050.py
@@ -4,3 +4,10 @@ res = 1
 for i in range(k):
     res *= (n+i)
 
+def C1(n):
+    if(n==0):
+        return 1
+    else:
+        return C1(n-1)*n
+
+print(int(C1(n)/(C1(k)*C1(n-k))))
--- Class2/2798.py
+++ Class2/2798.py
@@ -1,7 +1,14 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+lst = list(map(int, sys.stdin.readline().split()))
 
-N, M = map(int, input().split())
-lst = list(map(int, input().split()))
+res = 0
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            sum = lst[i] + lst[j] + lst[k]
+            if(sum<=M):
+                res = max(sum,res)
 
-lst.sort()
 
-
+print(res)
